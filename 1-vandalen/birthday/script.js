4d7f2bc2bc28e68fff61496cf0286ab890b2d094
holmewi <jh222qr@student.lnu.se>
2014-11-14 23:49:22 +0000

    Managed to sort out the leap year in a loop. Probably little over my head but still.

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -5,25 +5,103 @@ window.onload = function(){
 	
 	var birthday = function(date){
 		
-
-
 			// Din kod här.
 
 			var curDate = new Date();
-			var curDateTime = curDate.getTime();
+			var curDateTime = Math.ceil(curDate.getTime() / (1000*60*60*24));
 			var birthday = new Date(date);
-			var birthdayTime = new Date(date).getTime();
+			var birthdayTime = Math.ceil(birthday.getTime() / (1000*60*60*24));
+			var daysToBirthday = Math.ceil((birthdayTime - curDateTime));
+			var diffYear = curDate.getYear() - birthday.getYear();
+			
 			//console.log(curDate);
 			//console.log(curDateTime);
 			//console.log(birthday);
 			//console.log(birthdayTime);
+			//console.log(curDate.getFullYear());
+			//console.log(birthday.getFullYear());
+
+			//console.log(daysToBirthday);
 			
-			console.log(curDate.getMonth())
-			
-			if (birthday.getMonth() == curDate.getMonth() && birthday.getDay() == curDate.getDay()){
+			/*
+			if (birthday.getMonth() == curDate.getMonth() && birthday.getDate() == curDate.getDate()){
 				return 0;
 			}
+			if (birthday.getMonth() == curDate.getMonth() && birthday.getDate() == curDate.getDate() + 1){
+				return 1;
+			}
+			*/
+			if (curDate.getFullYear() != birthday.getFullYear()){
+				var leapYear = birthday.getFullYear();
+				for (var i = 0; i <= diffYear; ++i){
+					if (leapYear % 4 == 0) {
+				        console.log(leapYear);
+			        }
+			        leapYear++;
+			        
+				}
+					//console.log(birthday.getFullYear());
+					//console.log(birthday.getFullYear() % 4 == 0);
 			
+				birthday.setFullYear(curDate.getFullYear());
+				daysToBirthday = daysToBirthday + (365 * diffYear);
+			}
+			
+			/*
+			console.log(2014 % 4);
+			console.log(2013 % 4);
+			console.log(2012 % 4);
+			console.log(2011 % 4);
+			console.log(2010 % 4);
+			console.log(daysToBirthday);
+			console.log(birthday.getFullYear());
+			console.log(diffYear);
+			*/
+			
+			if (birthdayTime == curDateTime){
+				return 0;
+			}
+			if (birthdayTime == curDateTime + 1){
+				return 1;
+			}
+			
+			return daysToBirthday;
+			
+			
+			
+		
+		
+			/*
+			if (curDateTime - birthdayTime > 2){
+				birthday.setFullYear(curDate.setFullYear() + 1);
+				daysToBirthday = birthdayTime - curDateTime;
+				return daysToBirthday;
+			}
+			if (birthdayTime < curDateTime){
+				daysToBirthday = 365 - curDateTime - birthdayTime;
+				return daysToBirthday;
+			}
+					
+					var diffYear = curDate.getYear() - birthday.getYear();
+					birthday.setFullYear(curDate.setFullYear() + 1);
+					console.log(diffYear);
+					
+					if(diffYear >= 1){
+						var daysToBirthdayYear = Math.ceil(daysToBirthday - (diffYear * 365));
+						return daysToBirthdayYear;
+					}
+
+					if(diffYear <= 0){
+						return daysToBirthday;
+					}
+					*/
+					
+					
+					
+					
+					
+			
+				
 
 	};
 	// ------------------------------------------------------------------------------
