971e712cbd1f004b237a3da4f1998d7de2331423
holmewi <jh222qr@student.lnu.se>
2014-11-14 01:31:02 +0000

    Implemented if and else statements to determine uppercase and lowercase, also changing the 'a' and 'A' to a hashtag

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -2,27 +2,32 @@
 
 window.onload = function(){
 
-	
-	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var convertString = function(str){
 		// Plats för förändring.
+		var convChar = "";
 		
-		
-		if (input.lenght > 0) {
-			
-			
+		if (str.length > 0) {
+
+			for (var i = 0; i < str.length; i+= 1) {
+
+				// Talar om i fall bokstavskoden är en versal
+				// och gör i så fall om den till en gemen
+				// samt gör om alla 'a' till '#'
+				if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90 || str.charCodeAt(i) >= 196 && str.charCodeAt(i) <= 214) {
+					convChar += str.charAt(i).toLowerCase().replace(/a/g, "#");
+				// Om bokstaven inte är en versal
+				// Gör om till gemen samt ändra alla 'A' till '#'
+				} else {
+					convChar += str.charAt(i).toUpperCase().replace(/A/g, "#");
+				};
+			};
+			return convChar;
 		} else {
 			return "Du måste skriva något";
 		};
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
- 		
-
-
-
-
 
 	};
 	// ------------------------------------------------------------------------------
