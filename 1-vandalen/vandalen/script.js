7fa5ab09a3b6c75409b5376fb8dcc20c5850572a
holmewi <jh222qr@student.lnu.se>
2014-11-16 20:22:31 +0000

    Added the four properties and made the object return them

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,29 +3,30 @@
 var makePerson = function(persArr){
 
 	// Din kod här...
+	var result = {};
 	//console.log(persArr);
-	//var n = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(names){ return "Namn: " + names; });
-	
+
+	var minAge = Math.min(function(age){ return age;});
+	var maxAge = Math.max(function(age){ return age;});
+	var averageAge = Math.round(function(age){ return age;});
+	var names = persArr.map(function(name){ return name;});
+
+	result.minAge = minAge;
+	result.maxAge = maxAge;
+	result.averageAge = averageAge;
+	result.names = names;
+
+	console.log(result);
+	return result;
 	
 	/*
-	var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-	
-    var f = function(data){
-      	var obj = new Object();
-		obj.minAge = 36;
-		obj.maxAge = 46;
-		obj.avarageAge = 40;
-		obj.names = "John Häggerud, Johan Leitet, Mats Loock";
-    }
-    
-    
     {};
     */
-};
+}
 
 //var names = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(persArr){ return "Namn: " + persArr; });
 	
 // En array som innehåller argumenten name och age
-var persons = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
 // Skapar ett nytt objekt och skickar med arrayen
-var person = makePerson(persons){};
+var result = makePerson(data);
