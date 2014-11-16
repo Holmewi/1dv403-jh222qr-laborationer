5d3e1d7a52d34346830aade2ecc36c91f64c3e81
holmewi <jh222qr@student.lnu.se>
2014-11-16 19:21:09 +0000

    Added the array and created an new object that takes the array as an argument and finally returns the new object

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,30 @@
 
 var makePerson = function(persArr){
 
-
 	// Din kod här...
+	//console.log(persArr);
+	//var n = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(names){ return "Namn: " + names; });
+	
+	
+	/*
+	var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+	
+    var f = function(data){
+      	var obj = new Object();
+		obj.minAge = 36;
+		obj.maxAge = 46;
+		obj.avarageAge = 40;
+		obj.names = "John Häggerud, Johan Leitet, Mats Loock";
+    }
+    
+    
+    {};
+    */
+};
 
-}
-
+//var names = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(persArr){ return "Namn: " + persArr; });
+	
+// En array som innehåller argumenten name och age
+var persons = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+// Skapar ett nytt objekt och skickar med arrayen
+var person = makePerson(persons){};
