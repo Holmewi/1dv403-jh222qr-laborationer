1d629111a940d55408ff6630b19fb09fe9bc7bc4
holmewi <jh222qr@student.lnu.se>
2014-11-18 12:54:18 +0000

    Calculated the min, max and avarage age and send the properties to the result which returns it

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,25 +4,43 @@ var makePerson = function(persArr){
 
 	// Din kod här...
 	var result = {};
-	//console.log(persArr);
+	
+	var names;
+	names = persArr.map(function(name){ return name;});
 
-	var minAge = Math.min(function(age){ return age;});
-	var maxAge = Math.max(function(age){ return age;});
-	var averageAge = Math.round(function(age){ return age;});
-	var names = persArr.map(function(name){ return name;});
 
+	// Tar ut åldrarna från arrayen
+	// Varför age.age?
+	var ages = persArr.map(function(age){ return age.age;});
+
+	// Egenskaper och uträkning
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+	var minAge = Math.min.apply(this, ages);
+	var maxAge = Math.max.apply(this, ages);
+	
+	// Lägger ihop alla värderna i en array
+	// Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+	var averageAge = ages.reduce(function(a, b) { return a + b });
+	
+	// Rundar av till närmsta heltal
+	averageAge = Math.round(averageAge / ages.length);
+
+	//averageAge = Math.round(this, ages / ages.length);
+	console.log(averageAge);
+	
+	// Skickar in egenskaperna i objektet
 	result.minAge = minAge;
 	result.maxAge = maxAge;
 	result.averageAge = averageAge;
 	result.names = names;
 
-	console.log(result);
+	
 	return result;
 	
 	/*
     {};
     */
-}
+};
 
 //var names = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(persArr){ return "Namn: " + persArr; });
 	
@@ -30,3 +48,6 @@ var makePerson = function(persArr){
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
 // Skapar ett nytt objekt och skickar med arrayen
 var result = makePerson(data);
+
+console.log(result);
+console.log(ages);
