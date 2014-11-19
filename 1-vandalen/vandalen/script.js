c96ec92e311905e6e22db5f76e5fefcd813715dd
holmewi <jh222qr@student.lnu.se>
2014-11-19 19:17:11 +0000

    Created the error message when the expected value is false

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,20 +4,38 @@ var makePerson = function(persArr){
 
 	// Din kod här...
 	var result = {};
-	
+
 	// Tar ut åldrarna från arrayen och sorterar dem
 	// Sorterar även icke ASCII-bokstäver
 	// Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 	var names = persArr.map(function(name){ return name.name;}).sort(function(a, b){ return a.localeCompare(b)});
 	
+	// Tar ut åldrarna från arrayen
+	// Varför age.age?
+	var ages = persArr.map(function(age){ return age.age;});
+	
+	// Testar alla elementen i ages för att se om det är ett heltal
+	// Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+	var isNumber = ages.every(function isNumber(currentValue, index, array){
+		if (isNaN(currentValue)) {
+			console.log("En ålder är inte ett heltal!");
+		};
+	});
+
+	// Testar alla elementen i names för att se om det är en sträng
+	// Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+	var isString = names.every(function isString(currentString, index, array){
+		if (typeof currentString != 'string') {
+			console.log("Ett namn är inte en sträng!");
+		};
+	});
+
 	// Lägger ihop alla namn i arrayen och skiljer dem med ett kommatecken
 	// Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 	names = names.join(', ');
 	
-	// Tar ut åldrarna från arrayen
-	// Varför age.age?
-	var ages = persArr.map(function(age){ return age.age;});
-
+	// console.log(ages);
+	
 	// Egenskaper och uträkning
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
 	var minAge = Math.min.apply(this, ages);
@@ -29,8 +47,13 @@ var makePerson = function(persArr){
 	
 	// Rundar av till närmsta heltal
 	averageAge = Math.round(averageAge / ages.length);
-
-	//console.log(averageAge);
+	
+	// Skickar ett undantag om värdet inte är ett heltal
+	/*
+	if (isNaN(averageAge, minAge, maxAge)){
+		console.log("Värdet är inte ett heltal!");
+	}
+	*/
 	
 	// Skickar in egenskaperna i objektet
 	result.minAge = minAge;
@@ -38,18 +61,15 @@ var makePerson = function(persArr){
 	result.averageAge = averageAge;
 	result.names = names;
 
-	
 	return result;
 	
-	/*
-    {};
-    */
 };
 
-//var names = ["John Häggerud", "Johan Leitet", "Mats Loock", ].map(function(persArr){ return "Namn: " + persArr; });
-	
 // En array som innehåller argumenten name och age
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+
+	
 // Skapar ett nytt objekt och skickar med arrayen
 var result = makePerson(data);
 
