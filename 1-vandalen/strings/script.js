7f0a2c1b6eb2809b55e54b1e4123ec110e39c796
holmewi <jh222qr@student.lnu.se>
2014-11-26 11:37:02 +0000

    Made some minor changes the if statement to check if the character is uppercase

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -14,7 +14,18 @@ window.onload = function(){
 				// Talar om i fall bokstavskoden är en versal
 				// och gör i så fall om den till en gemen
 				// samt gör om alla 'a' till '#'
+				
+				/*
 				if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90 || str.charCodeAt(i) >= 196 && str.charCodeAt(i) <= 214) {
+					convChar += str.charAt(i).toLowerCase().replace(/a/g, "#");
+				// Om bokstaven inte är en versal
+				// Gör om till gemen samt ändra alla 'A' till '#'
+				} else {
+					convChar += str.charAt(i).toUpperCase().replace(/A/g, "#");
+				};
+				*/
+				
+				if (str.charAt(i) === str.charAt(i).toUpperCase()) {
 					convChar += str.charAt(i).toLowerCase().replace(/a/g, "#");
 				// Om bokstaven inte är en versal
 				// Gör om till gemen samt ändra alla 'A' till '#'
