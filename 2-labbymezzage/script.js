aa7232e1e959d3d1a08bbbdda5b1c6315e37b00b
holmewi <jh222qr@student.lnu.se>
2014-11-27 17:24:44 +0000

    Created the Message object and the function to send on click

---
+++ 2-labbymezzage/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Objektet
+function Message(textString) {
+    
+    
+    
+    
+    
+}
+
+window.onload = function() {
+    var submit = document.getElementById("send");
+    
+    submit.onclick = function() {
+ 
+        alert("TEST");
+        return false;
+    }
+    
+    
+}
