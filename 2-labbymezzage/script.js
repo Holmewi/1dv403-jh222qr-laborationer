4e532b4235df412e1ad800b7e269038b7c71e47a
holmewi <jh222qr@student.lnu.se>
2014-11-28 13:11:16 +0000

    Added the buttons in the aside and styled them in css

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -3,12 +3,12 @@
 // Objektet
 function Message(textString, date, dateTime) {
     
+    // Meddelandefönster
     var ul = document.getElementById("commentList");
     var li = document.createElement("li");
     var article = document.createElement("article");
     var aside = document.createElement("aside");
     var time = document.createElement("time");
-    
     
     ul.appendChild(li);
     li.appendChild(article);
@@ -17,8 +17,17 @@ function Message(textString, date, dateTime) {
     article.innerHTML = textString;
     time.innerHTML = dateTime;
     
-
+    // Aside-fönster
+    //aside.innerHTML += "<img src='pics/iconTime.png' class='showDate' alt='Show date'/><img src='pics/iconDelete.png' class='deleteMessage' alt='Delete message'/>";
+    var showDateButton = document.createElement("input");
+    showDateButton.type = "button";
+    showDateButton.setAttribute("id", "showDateButton");
+    var showDeleteButton = document.createElement("input");
+    showDeleteButton.type = "button";
+    showDeleteButton.setAttribute("id", "showDeleteButton");
     
+    aside.appendChild(showDateButton);
+    aside.appendChild(showDeleteButton);
     
 }
 
@@ -42,3 +51,4 @@ window.onload = function() {
 Date.prototype.dateTime = function () {
      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
 }
+
