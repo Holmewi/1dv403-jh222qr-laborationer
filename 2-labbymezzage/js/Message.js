380af7a00070f57c89349136e0e5d6fd6eee799c
holmewi <jh222qr@student.lnu.se>
2014-11-30 20:36:02 +0000

    Changed the input type textfield to a textarea to make the send onkeydown work when pressing enter

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -6,20 +6,27 @@ function Message(message, date){
     console.log(message);
     console.log(date);
     
-    this.getString = function() {
-        return message;
-    };
+    this.getString = function() { return message; };
+    this.setString = function(_textString) { message = _textString; };
+    this.getDate = function() { return date; };
+    this.setDate = function(_date) { date = _date; };
     
-    this.setString = function(_textString) {
-        message = _textString;
-    };
-    
-    this.getDate = function() {
-        return date;
-    };
-    
-    this.setDate = function(_date) {
-        date = _date;
-    };
+    this.setString(message);
+    this.setDate(date);
 }
 
+Message.prototype.showMessage = function(){
+    var ul = document.getElementById("commentList");
+    var li = document.createElement("li");
+    var article = document.createElement("article");
+    var aside = document.createElement("aside");
+    var time = document.createElement("time");
+    
+    ul.appendChild(li);
+    li.appendChild(article);
+    li.appendChild(aside);
+    li.appendChild(time);
+    article.innerHTML = this.getString();
+    time.innerHTML = this.getDate();
+ 
+};
