503191ceace30f1a5bf45d0930c3ff75a9b28870
holmewi <jh222qr@student.lnu.se>
2014-11-30 14:45:38 +0000

    Created a constructor to hold the message and date

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -1,6 +1,23 @@
 "use strict";
 
-// Konstruktor
-function Message(){
+// Konstruktor som tar hand om meddelandet och datum
+function Message(message, date){
     
+    console.log(message + date);
+    
+    this.getString = function() {
+        return message;
+    };
+    
+    this.setString = function(_textString) {
+        message = _textString;
+    };
+    
+    this.getDate = function() {
+        return date;
+    };
+    
+    this.setDate = function(_date) {
+        date = _date;
+    };
 }
