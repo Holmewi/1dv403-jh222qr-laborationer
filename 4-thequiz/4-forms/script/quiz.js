723b09fa06d2ca625ad3b8ef6dbcd85d6523a621
holmewi <jh222qr@student.lnu.se>
2014-12-14 19:37:42 +0000

    Finally managed to get the responses from the servers right to view the question

--- 4-thequiz/4-forms/script/quiz.js
+++ 4-thequiz/4-forms/script/quiz.js
@@ -1,42 +1,76 @@
 "use strict";
 
 var Quiz = {
+    url : "http://vhost3.lnu.se:20080/question/1",
+    count : 0,
+    tries : [],
+
     init : function() {
-        console.log("test");
-        Quiz.createQuiz();
+        document.getElementById('startQuiz').addEventListener('click', function(){
+            var xhr = new XMLHttpRequest();
+            Quiz.createBoard();
+            Quiz.getResponse(Quiz.url, xhr);
+        });
     },
     
-    createQuiz : function() {
+    createBoard : function() {
         document.getElementById('startQuiz').setAttribute('class', 'hidden');
         var quizNav = document.getElementById('quizNav');
         var quizBoard = document.getElementById('quizBoard');
         
         // Frågan
-        var quesion = document.createElement('p');
-        quesion.setAttribute('class', 'question');
-        quizBoard.appendChild(quesion);
+        var question = document.createElement('p');
+        question.setAttribute('class', 'question');
+        question.setAttribute('id', 'question');
+        quizBoard.appendChild(question);
+        
         // Svarsinput
         var answer = document.createElement('input');
         answer.setAttribute('type', 'text');
         answer.setAttribute('id', 'answer');
         quizBoard.appendChild(answer).focus();
         
-        // Svaraknappen
+        // Svarsknappen
         var button = document.createElement('input');
         button.setAttribute('type','button');
         button.setAttribute('value','Svara');
+        button.setAttribute('id', 'button');
         button.setAttribute('class', 'button');
         quizNav.appendChild(button);
+        
+        
     },
     
-    getQuestion : function() {
+    getResponse : function(url, xhr) {
         
-    }
-}
+        xhr.onreadystatechange = function(){
+			if(xhr.readyState === 4){
+				if(xhr.status == 200){
+					Quiz.renderQuestion(JSON.parse(xhr.responseText));
+					Quiz.postAnswer(JSON.parse(xhr.responseText));
+				}
+			} else {
+				console.log("Läsfel, status:"+xhr.status);
+			}
+		};
 
-window.onload = function(e) {
-    document.getElementById('startQuiz').addEventListener('click', function(){
-        e.preventDefault();
-        Quiz.init();
-    });
+		xhr.open("GET", url , true);
+		xhr.send(null);
+    },
+    
+    renderQuestion: function(response){
+		var text = document.querySelector(".question");
+		
+		if(response.question){
+			text.innerHTML = response.question;
+		} else {
+			text.innerHTML = response.message;
+		}
+	},
+    
+    
+};
+
+window.onload = function() {
+    Quiz.init();
 };
