1ed10b5a1c0896c2a31a2317e015180c762942ae
holmewi <jh222qr@student.lnu.se>
2014-12-14 14:33:26 +0000

    Added a function that will create the quizboard and its elements

--- 4-thequiz/4-forms/script/quiz.js
+++ 4-thequiz/4-forms/script/quiz.js
@@ -3,12 +3,40 @@
 var Quiz = {
     init : function() {
         console.log("test");
-        document.getElementById("startQuiz").setAttribute("class", "hidden");
+        Quiz.createQuiz();
+    },
+    
+    createQuiz : function() {
+        document.getElementById('startQuiz').setAttribute('class', 'hidden');
+        var quizNav = document.getElementById('quizNav');
+        var quizBoard = document.getElementById('quizBoard');
+        
+        // Frågan
+        var quesion = document.createElement('p');
+        quesion.setAttribute('class', 'question');
+        quizBoard.appendChild(quesion);
+        // Svarsinput
+        var answer = document.createElement('input');
+        answer.setAttribute('type', 'text');
+        answer.setAttribute('id', 'answer');
+        quizBoard.appendChild(answer).focus();
+        
+        // Svaraknappen
+        var button = document.createElement('input');
+        button.setAttribute('type','button');
+        button.setAttribute('value','Svara');
+        button.setAttribute('class', 'button');
+        quizNav.appendChild(button);
+    },
+    
+    getQuestion : function() {
+        
     }
 }
 
-window.onload = function() {
-    document.getElementById("startQuiz").addEventListener("click", function(){
+window.onload = function(e) {
+    document.getElementById('startQuiz').addEventListener('click', function(){
+        e.preventDefault();
         Quiz.init();
     });
 };
