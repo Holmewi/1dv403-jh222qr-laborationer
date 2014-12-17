70c89d1d456cb738494704d2efc3db2382362223
holmewi <jh222qr@student.lnu.se>
2014-12-17 18:15:17 +0000

    Changed the addEventHandler to onclick instead, to get it to work properly

--- 4-thequiz/script/quiz.js
+++ 4-thequiz/script/quiz.js
@@ -6,10 +6,10 @@ var Quiz = {
     tries : [],
 
     init : function() {
-        document.getElementById('startQuiz').addEventListener('click', function(){
+        document.getElementById('startQuiz').onclick = function(){
             Quiz.createBoard();
             Quiz.getResponse("http://vhost3.lnu.se:20080/question/1");
-        });
+        };
     },
     
     createBoard : function() {
@@ -46,32 +46,35 @@ var Quiz = {
 			if(xhr.readyState === 4){
 				if(xhr.status == 200){
 					Quiz.renderQuestion(JSON.parse(xhr.responseText));
-                    Quiz.sendAnswer(JSON.parse(xhr.responseText), xhr);
+                    Quiz.sendAnswer(JSON.parse(xhr.responseText), url);
+                    console.log("TESTAR: " + xhr.responseText);
 				} else {
 				console.log("Läsfel, status:"+xhr.status);
 			    }
 			} 
 		};
 
-		xhr.open("GET", url , true);
+		xhr.open('GET', url , true);
 		xhr.send(null);
     },
 	
-	sendAnswer : function(response, xhr) {
+	sendAnswer : function(response, url) {
 	    var answer = document.querySelector('#answer');
 	    
-	    document.getElementById('subAnswer').addEventListener('click', function(){
-	        
+	    document.getElementById('subAnswer').onclick = function(){
+	        var xhr = new XMLHttpRequest();
 	        Quiz.count += 1;
 	        console.log(Quiz.count);
 	        console.log(answer.value);
+	        console.log(response);
+	        
 	        
 	        xhr.onreadystatechange = function(){
 	            
 	            if(xhr.readyState === 4){
 	                
 	                if(xhr.status == 200){
-	                    
+	                    console.log(xhr.responseText);
 	                    response = JSON.parse(xhr.responseText);
 	                    Quiz.tries.push(Quiz.count);
 	                    Quiz.count = 0;
@@ -84,6 +87,7 @@ var Quiz = {
                             
                             setTimeout(function(){
                                 Quiz.getResponse(response.nextURL);
+                                console.log("URL :" + response.nextURL);
                             }, 1000);
                             
                             
@@ -92,16 +96,18 @@ var Quiz = {
 							console.log("SLUT");
 						}
                     } else if(xhr.status == 400) {
+                        
 						console.log("FEL SVAR");
 						Quiz.renderQuestion(JSON.parse(xhr.responseText));
                         document.getElementById("answer").focus();
                         setTimeout(function(){
                             answer.value = "";
-                            console.log("STATUS2" + xhr.status);
+                            console.log("STATUS2 " + xhr.status);
                             Quiz.renderQuestion(response);
                         }, 1000);
 			        } else {
                     console.log("Läsfel, status:"+xhr.status);
+                    console.log(xhr.responseText);
 				    }
                 } 
 	        };
@@ -111,9 +117,10 @@ var Quiz = {
 	        });
 	        
 	        xhr.open('POST', response.nextURL, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
+	        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            //xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(input); 
-        });
+        };
 	},
 	
 	renderQuestion: function(response) {
@@ -128,12 +135,16 @@ var Quiz = {
 	viewResult : function() {
 	    var quizNav = document.getElementById('quizNav');
 	    var text = document.querySelector('.question');
+	    var hr = document.createElement('hr');
+	    quizNav.appendChild(hr);
 		text.innerHTML = "The game is over";
 
 		for (var i = 0; i < Quiz.tries.length; i += 1) {
 			var p = document.createElement('p');
+			p.setAttribute('class', 'result');
 			quizNav.appendChild(p);
-			p.innerHTML = "You needed " + Quiz.tries[i] + " tries on question " + (i + 1) + ".";
+			p.innerHTML = "You needed " + Quiz.tries[i] + " attept(s) on question " + (i + 1) + ".";
+			//p.innerHTML = "Question " + (i + 1) + ", " + Quiz.tries[i] + " Attept(s)";
 		}
 	}
     
